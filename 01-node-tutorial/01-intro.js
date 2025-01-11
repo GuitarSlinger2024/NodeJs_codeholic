9985ffec9ac25704eeaa7f3085abca97c04a0731
Andrew James <pickhandfirstaid@gmail.com>
2025-01-11 17:34:58 -0600

    general update

--- 01-node-tutorial/01-intro.js
+++ 01-node-tutorial/01-intro.js
@@ -1,4 +1,7 @@
-const amount = 9
+//  The YouTube video for this course is at:
+//  https://www.youtube.com/watch?v=Oe421EPjeBE&t=27s
+
+const amount = 12
 
 if (amount < 10) {
   console.log('small number')
@@ -6,4 +9,4 @@ if (amount < 10) {
   console.log('large number')
 }
 
-console.log(`hey it's my first node app!!!`)
+console.log(`hey, it's my first node app!!!`)
