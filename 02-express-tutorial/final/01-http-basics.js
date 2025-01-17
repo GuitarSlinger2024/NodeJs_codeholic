31a71c1e3303823db3db2ff1ed55f3a65c811bf6
Andrew James <pickhandfirstaid@gmail.com>
2025-01-16 22:18:44 -0600

    general update

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -6,19 +6,19 @@ const server = http.createServer((req, res) => {
   // home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
+    res.write('<h1>home page</h1><a href="/about">About</a>')
     res.end()
   }
   // about page
   else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
+    res.write('<h1>about page</h1><a href="/">Home</a>')
     res.end()
   }
   // 404
   else {
     res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
+    res.write('<h1>page not found</h1><a href="/">Home</a>')
     res.end()
   }
 })
